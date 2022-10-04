a790f5234062658b2c0f7dd8ee4b01fb18cdd330
Douglas Ribeiro <dgrb0325@gmail.com>
2022-10-03 21:58:10 -0300

    fixed code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate } = require("uuid");
 
 const app = express();
 
@@ -13,24 +13,40 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
     title,
     url,
     techs,
-    likes: 0
+    likes: 0,
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
+  const isIdValid = validate(id);
+
+  if (!isIdValid) {
+    return response.status(404).json({ error: "Invalid id" });
+  }
+
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const hasLikesInProperties = updatedRepository.hasOwnProperty("likes");
+
+  if (hasLikesInProperties) {
+    delete updatedRepository.likes;
+  }
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -45,10 +61,17 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
+  const isIdValid = validate(id);
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (!isIdValid) {
+    return response.status(404).json({ error: "Invalid id" });
+  }
 
-  if (repositoryIndex > 0) {
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -59,16 +82,23 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
+  const isIdValid = validate(id);
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (!isIdValid) {
+    return response.status(404).json({ error: "Invalid id" });
+  }
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
